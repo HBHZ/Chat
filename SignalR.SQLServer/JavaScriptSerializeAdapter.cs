c90522905767395d5b71256b1f765bd62416b6be
HBHZ <3026582896@qq.com>
2016-06-04 17:40:49 +0800

    Create JavaScriptSerializeAdapter.cs

---
+++ SignalR.SQLServer/JavaScriptSerializeAdapter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SignalR;
+using System.Web.Script.Serialization;
+
+namespace SignalR.SQL {
+      internal class JavaScriptSerializerAdapter : IJsonSerializer {
+          private JavaScriptSerializer _serializer;
+          
+          public JavaScriptSerializerAdapter(JavaScriptSerializer serializer) {
+              _serializer = serializer;
+          }
+          
+          public string Stringfy(object obj) {
+              return _serializer.Serialize(obj);
+          }
+          
+          public object Parse(string json) {
+              return _serializer.DeserializeObject(json);
+          }
+          
+          public object Parse(string json, Type targetType) {
+              return _serializer.Deserialize(json,targetType);
+          }
+          public T Parse<T>(string json) {
+              return _serializer.Deserialize<T>(json);
+          }
+        }
+  }
