51a7972725e8adbc8ba4281af7399b489d1ee052
HBHZ <3026582896@qq.com>
2016-06-04 17:14:20 +0800

    Update IJsonSerializer.cs

--- SignalR.SQLServer/IJsonSerializer.cs
+++ SignalR.SQLServer/IJsonSerializer.cs
@@ -1,1 +1,11 @@
+using System;
 
+namespace SignalR {
+    public interface IJsonSerializer : IJsonStringifier {
+      object Parse(string json);
+      
+      object Parse(string json, Type targetType);
+      
+      T Parse<T>(string json);
+    }
+}
