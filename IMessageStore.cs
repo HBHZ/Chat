89e107ab0b40c5ebd95c260c99b83498bbe056e0
HBHZ <3026582896@qq.com>
2016-06-06 11:55:50 +0800

    Create IMessageStore.cs

---
+++ IMessageStore.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalR {
+    public interface IMessageStore {
+          Task<long?>GetLastId();
+          Task Save(string key, object value);
+          Task<IEnumerable<Message>> GetAll(string key);
+          Task<IEnumerable<Message>> GetAllSince(string key, long id);
+    }
+}
