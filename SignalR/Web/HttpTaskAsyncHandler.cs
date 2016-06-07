b5c6e92d0d09f65e2657f2aafad9683f1678bdd2
HBHZ <3026582896@qq.com>
2016-06-07 17:47:39 +0800

    Update HttpTaskAsyncHandler.cs

--- SignalR/Web/HttpTaskAsyncHandler.cs
+++ SignalR/Web/HttpTaskAsyncHandler.cs
@@ -1,1 +1,24 @@
 
+using System;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalR.Web {
+    public abstract class HttpTaskAsyncHandler : IHttpAsyncHandler {
+        public virtual boll IsReusable{
+            get { return false;}
+        }
+        public virtual void ProcessReqest(HttpContext context){
+            throw new NotSupportedEXcetion();
+        }
+        public agstract Task ProcessRequestAsync(HttpContext context);
+        
+        IAsyncResult IHttpAsyncHandler.BeginProcessRequest(HttpContext context, AsyncCallback cb, object extraData){
+          return TaskAsyncHelper.BeginTask() => ProcessRequestAsync(context), cb, extraData);
+          
+        }
+        void IHttpAsyncHandler.EndProcessRequest(IasyncResult result){
+            TaskAsyncHelper.EndTask(result);
+        }
+    }
+}
